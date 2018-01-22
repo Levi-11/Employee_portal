6f7cf01adc4053e48e9113a98b5aeee4fcc13625
Levi-11 <lvaguez@gmail.com>
2018-01-22 12:02:10 +0530

    fb v2

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -9,24 +9,28 @@ class EmployeesController < ApplicationController
     @employee = current_employee
   end
   
+  
   def edit 
-     
-     @employee = current_employee
-    #  if FACEBOOK_CONFIG.blank?
-      
-      if params["fb"]
-        
-        @user = Employee.koala(request.env['omniauth.auth']['credentials'])
-        if @user['email'] || @user['birthday']
-          @employee.update_attributes(personalemail: @user['email'], 
-          dateofbirth: date_converter(@user['birthday']) )
-          redirect_to home_path,notice: "successfully updated"
-        else
-          redirect_to edit_employee_path(@employee),notice: "updation failed"
-        end
-      else
-        render 'employees/edit'
-      end
+    @employee = current_employee
+    if params["fb"]
+      config = request.env['omniauth.auth']['credentials']
+      @graph = Koala::Facebook::API.new(config['token'])
+      @user = @graph.get_object('me?fields=picture,name,email,birthday,hometown,location,posts')
+      @employee.update_attributes(fb_email: @user['email'],
+                                  fb_birthday: date_converter(@user['birthday']),
+                                  fb_name: @user['name'],
+                                  picture: @user['picture']['data']['url'],
+                                  home_town: @user['hometown']['name'],
+                                  fb_location: @user['location']['name'],
+                                  fb_posts: @user['posts']['data'][0]['story'] || @user['posts']['data'][0]['message'])
+      #   flash[:success] = "Successfully Updated the Profile with facebook details!"
+      #   redirect_to home_path
+      # else
+      #   redirect_to edit_employee_path(@employee), notice:"Updation failed"
+      # end
+    else
+      render "edit"
+    end
   end
   
   def update
--- test/integration/employees_edit_test.rb
+++ test/integration/employees_edit_test.rb
@@ -4,21 +4,18 @@ class EmployeesEditTest < ActionDispatch::IntegrationTest
   def setup
     @employee = employees(:example)
     OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-      :provider => 'facebook',
-      :uid => '123545',
-      :info => {
-        :personalemail => 'lvaguez@gmail.com',
-        :dateofbirth => '17/04/1993'
-      },
-      :credentials => {
-                        token: 'EAACEdEose0cBAJCbuAcrfwZCCvZCssH7oevrfoEdahTCcWcqLHx1YNZAD79gtPIV5bziXZBKSiQLXpcksEZB2Yr98R33tnXWzTyfPQdTpFevn9ZBqviZAJfdVZBa3ZAdkBHESmzUMTZBVR6CRRP8tXSvxZC5zfIRNj3W7y7QqkigcLL5g6MCS18ptWE4zyqRDnT1Y4UpVIX71E5sgZDZD',
-                        secret: 'dda912d5df718404401351518ce41469'
-                      }
-      
+                                          :provider => 'facebook',
+                                          :uid => '123545',
+                                          :info => {
+                                              :name => ' Example',
+                                              :email => 'example@gmail.com',
+                                              :birthday => '01/10/1995'
+                                            },
+                                            :credentials => {
+                                              token: 'EAACQocJY0vUBAPTCE6GQGuQrJqmyvN78pQZBjkWs2HFjH6pnD3AkSq64N2M7ZACQaZADHeyzGhoQJNdqSuc47AydMdyfJmXrHo4ypvwCP3eYJcpf5AJzMKbI7zh0vKdOIHVXM3eU8LeWo5EQz776cDUHIVbZAgqfGfMsmLswFDMv0AR8WE84LfFLtl1W0sQZD',
+                                              secret: 'ea0245ec077f79112a5be084ce5690bd'
+                                            }
     })
-    stub_request(:get, /https:\/\/graph\.facebook\.com\/v2\.4\/me\?(access_token=[\S]*&)fields=id,name,about,birthday,email,picture/).
-  with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.12.2'}).
-  to_return(status: 200, body: {name: 'Nikhil', birthday: '10/10/1993'}.to_json, headers: {})
   end
 
   test "unsuccessful edit" do    
@@ -27,7 +24,7 @@ class EmployeesEditTest < ActionDispatch::IntegrationTest
                                           password: 'password' } }
     get edit_employee_path(@employee)
     assert_template 'employees/edit'
-    assert_select 'a[href=?]', auth_provider_path
+    assert_select 'a[href=?]',auth_provider_path
     get auth_provider_path
     assert_redirected_to auth_facebook_callback_path
     patch employee_path(@employee), params: { employee: { 
@@ -47,8 +44,6 @@ class EmployeesEditTest < ActionDispatch::IntegrationTest
                                           password: 'password' } }
     get edit_employee_path(@employee)
     assert_template 'employees/edit'
-    assert_select 'a[href=?]', auth_provider_path
-
     email = "foo@bar.com"
     designation="developer",
     dateofjoin="12/11/2017",
@@ -64,12 +59,13 @@ class EmployeesEditTest < ActionDispatch::IntegrationTest
                                               username:username } }
     assert_not flash.empty?
     assert_redirected_to home_path
-    get auth_provider_path
-    assert_redirected_to auth_facebook_callback_path
-    follow_redirect!
+    @employee.reload 
   end
 
-  test "facebook login success" do
+  test "successful facebook update (only birthday)" do
+    stub_request(:get, /https:\/\/graph\.facebook.com\/v2\.0\/me\?(access_token=[A-Za-z0-9]*&)?fields=picture,name,email,birthday/).
+    with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.12.2'}).
+    to_return(status: 200, body: {birthday: '01/10/1995'}.to_json, headers: {})
     get login_path
     post login_path, params: { session: { email:    @employee.email,
                                           password: 'password' } }
@@ -79,16 +75,47 @@ class EmployeesEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to home_path
   end
 
-  test "facebook login not success" do
-    stub_request(:get, /https:\/\/graph\.facebook\.com\/v2\.4\/me\?(access_token=[\S]*&)fields=id,name,about,birthday,email,picture/).
+  test "successful facebook update (only email)" do
+    stub_request(:get, /https:\/\/graph\.facebook.com\/v2\.0\/me\?(access_token=[A-Za-z0-9]*&)?fields=picture,name,email,birthday/).
     with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.12.2'}).
-    to_return(status: 200, body: {name: 'Nikhil'}.to_json, headers: {})
+    to_return(status: 200, body: {email: 'ancy@gmail.com'}.to_json, headers: {})
     get login_path
     post login_path, params: { session: { email:    @employee.email,
                                           password: 'password' } }
     get auth_provider_path
     assert_redirected_to auth_facebook_callback_path
     follow_redirect!
+    assert_redirected_to home_path
+  end
+
+  test "successful facebook update (both email and birthday)" do
+    stub_request(:get, /https:\/\/graph\.facebook.com\/v2\.0\/me\?(access_token=[A-Za-z0-9]*&)?fields=picture,name,email,birthday/).
+    with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.12.2'}).
+    to_return(status: 200, body: {email: 'ancy@gmail.com',birthday:'01/10/1995'}.to_json, headers: {})
+    get login_path
+    post login_path, params: { session: { email:    @employee.email,
+                                          password: 'password' } }
+    get auth_provider_path
+    assert_redirected_to auth_facebook_callback_path
+    follow_redirect!
+    assert_redirected_to home_path
+  end
+
+  test "invalid update with facebook" do
+    stub_request(:get, /https:\/\/graph\.facebook.com\/v2\.0\/me\?(access_token=[A-Za-z0-9]*&)?fields=picture,name,email,birthday/).
+    with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.12.2'}).
+    to_return(status: 200, body: {name: 'Ancy'}.to_json, headers: {})
+    get login_path
+    post login_path, params: { session: { email:    @employee.email,
+                                          password: 'password' } }
+    
+    get edit_employee_path(@employee)
+    assert_template 'employees/edit'
+    assert_select 'a[href=?]',auth_provider_path
+    get auth_provider_path
+    assert_redirected_to auth_facebook_callback_path
+    follow_redirect!
     assert_redirected_to edit_employee_path(@employee)
   end
+
 end
